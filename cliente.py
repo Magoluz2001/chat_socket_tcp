99dc5b8ca5b4a973bee9ea3d57653ee8fac53bfd
Magoluz2001 <magoluz200@unifesspa.edu.br>
2022-10-07 01:00:57 -0300

    Chat versão final

--- cliente.py
+++ cliente.py
@@ -1,19 +1,33 @@
+from asyncore import write
+from http import client
 import socket
+import threading
+
+username = input('Escolha um nome de usuário: ')
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 cliente.connect(('localhost', 8888))
 
-terminado = False
+def recebe():
+    while (True):
+        try:
+            mensagem = cliente.recv(1024).decode('utf-8')
+            if (mensagem == 'UN'):
+                cliente.send(username.encode('utf-8'))
+            else:
+                print(mensagem)
+        except:
+            print('Ocorreu um erro.')
+            cliente.close()
+            break
 
-print("Digite 'finalizar chat' para encerrar a conexão.")
-while not(terminado):
-    cliente.send(input('Mensagem: ').encode('utf-8'))
-    msg = cliente.recv(1024).decode('utf-8')
+def escreve():
+    while (True):
+        mensagem = f"{username}: {input('')}"
+        cliente.send(mensagem.encode('utf-8'))
 
-    if (msg == 'finalizar chat'):
-        terminado = True
-    else:
-        print(f"Servidor: {msg}")
+recebe_thread = threading.Thread(target=recebe)
+recebe_thread.start()
 
-cliente.close()
+escreve_thread = threading.Thread(target=escreve)
+escreve_thread.start()
--- servidor.py
+++ servidor.py
@@ -1,26 +1,49 @@
+from concurrent.futures import thread
 import socket
+import threading
 
-# Criação do servidor
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.bind(('localhost', 8888))
-
+servidor.bind(("localhost", 8888))
 servidor.listen()
-cliente, endereco = servidor.accept()
 
-# Declaração de variávies
-terminado = False
+clientes = []
+usernames = []
 
-# Main
+def transmitir(mensagem):
+    for cliente in clientes:
+        cliente.send(mensagem)
 
-while not(terminado):
-    msg = cliente.recv(1024).decode('utf-8')
-    
-    if (msg == 'finalizar chat'):
-        terminado = True
-    else:
-        print(f"Cliente: {msg}")
-    
-    cliente.send(input('Mensagem: ').encode('utf-8'))
+def gerencia(cliente):
+    while (True):
+        try:
+            mensagem = cliente.recv(1024)
+            transmitir(mensagem)
+        except:
+            index = clientes.index(cliente)
+            clientes.remove(cliente)
+            cliente.close()
+            username = usernames[index]
+            transmitir(f"{username} saiu do chat.".encode('utf-8'))
+            usernames.remove(username)
+            break
 
-servidor.close()
-cliente.close()
+def recebe():
+    while (True):
+        cliente, endereco = servidor.accept()
+        print(f"Conectado com {endereco}")
+
+        cliente.send("UN".encode('utf-8'))
+        username = cliente.recv(1024).decode('utf-8')
+        usernames.append(username)
+        clientes.append(cliente)
+
+        print(f"Nome de usuário do cliente é {username}")
+
+        transmitir(f"{username} se conectou ao servidor".encode('utf-8'))
+        cliente.send("Conectado ao servidor.".encode('utf-8'))
+
+        thread = threading.Thread(target=gerencia, args=(cliente,))
+        thread.start()
+        
+print("O servidor está escutando...")
+recebe()
